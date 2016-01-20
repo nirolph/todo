e047eb5cf4cc039e3e7285f09bbadd451c840817
florin.d <florin.dragota@gmail.com>
2016-01-20 07:42:30 +0200

    added EntityMustValidateException

---
+++ Domain/Context/Task/Exception/EntityMustValidateExeption.php
@@ -0,0 +1,12 @@
+<?php
+namespace Domain\Context\Task\Exception;
+
+/**
+ * Description of EntityMustValidateExeption
+ *
+ * @author florin
+ */
+class EntityMustValidateExeption extends BaseException
+{
+    //put your code here
+}
